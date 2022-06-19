8ddbc8a3fc72eb9ef1c403c70a9d321406b7578d
vincent <vincent@ducamps.win>
2022-06-19 16:04:32 +0200

    add terragorm provissionning for VPS

---
+++ infra/server.tf
@@ -0,0 +1,11 @@
+resource "hcloud_server" "HomeLab" {
+  count       = var.instances
+  name        = "random"
+  image       = var.os_type
+  server_type = var.server_type
+  location    = var.location
+  ssh_keys     = [hcloud_ssh_key.default.id]
+  labels = {
+  }
+
+}
---
+++ infra/variable.tf
@@ -0,0 +1,19 @@
+variable "hcloud_token" {
+  # default = <your-api-token>
+}
+
+variable "location" {
+  default = "hel1"
+}
+variable "instances" {
+  default = "1"
+}
+
+variable "server_type" {
+  default = "cpx11"
+}
+
+variable "os_type" {
+  default = "rocky-8"
+}
+
