1b4fafb42267af8563a83af3fa296b34dbb863d9
vincent <vincent@ducamps.win>
2022-07-26 21:04:32 +0200

    switch backend to condul

--- infra/providers.tf
+++ infra/providers.tf
@@ -4,7 +4,10 @@ terraform {
       source = "hetznercloud/hcloud"
     }
   }
-  required_version = ">= 0.13"
+   backend "consul" {
+    path  = "terraform/infra"
+  }
+ required_version = ">= 0.13"
 }
 
 provider "hcloud" {
